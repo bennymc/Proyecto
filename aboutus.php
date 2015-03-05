29abe65b74ac42ead149c03bfd69b54320c9eb2c
bennymc <benny_9_3@hotmail.com>
2015-03-05 08:15:22 -0600

    Nombres en about us

--- aboutus.php
+++ aboutus.php
@@ -2,12 +2,14 @@
 	include "includes/navbar.html";
 ?>
 <div id="contenedor">		
-		<div class="panel panel-info col-md-4 col-md-offset-4">
+		<div class="panel panel-info col-md-4 col-md-offset-4" id="aboutus">
 		  <div class="panel-heading">
 		    <h3 class="panel-title">CREADORES</h3>
 		  </div>
 		  <div class="panel-body">
-		    FALTA ACTUALIZAR
+		    <p>Integrantes del equipo: </p>
+		    <p>Juan Bernardo Morales Castañeda</p>
+		    <p>Melissa Rubi De Alba Mendoza</p>
 		  </div>
 		</div>
 </div>
