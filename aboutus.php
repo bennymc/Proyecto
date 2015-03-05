e49a07a4be2e5cfb10e2dac33b0d7983b7c0aa19
Melissa De Alba <Rubi_93_22@hotmail.com>
2015-03-04 21:11:53 -0600

    ABOUT US INCOMPLETO

---
+++ aboutus.php
@@ -0,0 +1,19 @@
+<?php
+	include "includes/navbar.html";
+?>
+		
+		<div class="panel panel-info col-md-4 col-md-offset-4">
+		  <div class="panel-heading">
+		    <h3 class="panel-title">CREADORES</h3>
+		  </div>
+		  <div class="panel-body">
+		    FALTA ACTUALIZAR
+		  </div>
+		</div>
+
+<?php
+	include "includes/footer.html";
+?>
+	   
+    </body>
+</html>
