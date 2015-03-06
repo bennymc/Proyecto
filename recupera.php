665738eca1f147598efea7060dcccf90d657aa4a
Melissa De Alba <Rubi_93_22@hotmail.com>
2015-03-06 10:52:28 -0600

    Arregle formulario para recuperar contraseña
    boton enviar a la derecha , agrege margen arriba del formulario

--- recupera.php
+++ recupera.php
@@ -1,33 +1,35 @@
 <?php
 	include "includes/navbar.html";
 ?>
-<div id="contenedor">
-	<section class="col-md-4 col-md-offset-4">
-		<div class="panel panel-danger">
-			<div class="panel-heading">
-	   			 <h3 id="recuperarpass" class="panel-title">Recuperar Contraseña</h3>
-	  		</div>
-	  		<form>
-	  		<div class="panel-body">
-	   			<div class="form-group">
-					  <label class="col-md-4 control-label" for="inputrecuperapass">Ingresa tu Usuario o Correo electronico:</label>
-					  <div class="col-md-5">
-					    <input id="inputrecuperapass" name="inputrecuperapass" placeholder="" class="form-control input-md" required="">
-					  </div>
-					</div>
-	  		
-		  		<div class="form-group">
-					<div class="col-md-6 col-md-offset-1">
-					<button type="submit" class="btn btn-danger" id="btnrecupera">Enviar</button>
-					</div>
-				</div>	
-	  		</div>	
-	  		</form>
-		</div>
-	</section>
-</div>
-		<?php
-			include "includes/footer.html";
-		?>
+	
+	<div id="contenedor">
+		<section id="formrecuperapass" class="col-md-4 col-md-offset-4">
+			<div class="panel panel-danger">
+				<div class="panel-heading">
+		   			 <h3 id="recuperarpass" class="panel-title">Recuperar Contraseña</h3>
+		  		</div>
+		  		<form>
+			  		<div class="panel-body">
+			   			<div class="form-group">
+							  <label class="col-md-12 control-label" for="inputrecuperapass">Ingresa tu Usuario o Correo electronico:</label>
+							  <div class="col-md-12">
+							    <input id="inputrecuperapass" name="inputrecuperapass" placeholder="" class="form-control input-md" required="">
+							  </div>
+						</div>
+			  		
+				  		<div class="form-group">
+							<div class="col-md-12 ">
+							<button type="submit" class="btn btn-danger pull-right" id="btnrecupera">Recuperar</button>
+							</div>
+						</div>	
+			  		</div>	
+		  		</form>
+			</div>
+		</section>
+	</div>
+
+<?php
+	include "includes/footer.html";
+?>
 
 
