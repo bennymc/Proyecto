61d1ea4241afe6eb158fd801ecab7860cc94f2a7
Melissa De Alba <Rubi_93_22@hotmail.com>
2015-03-06 23:24:09 -0600

    separe las imagenes de los libros en generos
    en la pagina de generos puse margen a las imagenes para que se separen y
    agrege un link vermas , que nos evia a la pagina de X genero

--- libros.php
+++ libros.php
@@ -1,98 +1,122 @@
 <?php
 	include "includes/navbar.html";
 ?>
+<!-- ******************** CATALOGO DE LIBROS ************************** -->
+<!-- ******************** INICIA SECTION ************************** -->
 	<section class="col-md-7">
+	<!-- ******************** GENERO 1 ************************** -->	
 		<div class="panel panel-success">
   			<div class="panel-heading">
-    			<a href="genero.php"><h3 class="panel-title" id="grande">Genero 1</h3></a>
+    			<a href="genero.php"><h3 class="TituloGrande">Genero 1</h3></a>
   			</div>
-  			<div class="panel-body">
-				<a href="ejemplar.php"><img src="http://placehold.it/100x100" alt="Libro"></a>
-				<a href="ejemplar.php"><img src="http://placehold.it/100x100" alt="Libro"></a>
-				<a href="ejemplar.php"><img src="http://placehold.it/100x100" alt="Libro"></a>
-				<a href="ejemplar.php"><img src="http://placehold.it/100x100" alt="Libro"></a>
-				<a href="ejemplar.php"><img src="http://placehold.it/100x100" alt="Libro"></a>
-				<a href="ejemplar.php"><img src="http://placehold.it/100x100" alt="Libro"></a>
-				<a href="ejemplar.php"><img src="http://placehold.it/100x100" alt="Libro"></a>
-				<a href="ejemplar.php"><img src="http://placehold.it/100x100" alt="Libro"></a>
-				<a href="ejemplar.php"><img src="http://placehold.it/100x100" alt="Libro"></a>
-				<a href="ejemplar.php"><img src="http://placehold.it/100x100" alt="Libro"></a>
+  			<div class="panel-body librosSeparados">
+				<a href="ejemplar.php"><img src="http://placehold.it/100x150" alt="Libro"></a>
+				<a href="ejemplar.php"><img src="http://placehold.it/100x150" alt="Libro"></a>
+				<a href="ejemplar.php"><img src="http://placehold.it/100x150" alt="Libro"></a>
+				<a href="ejemplar.php"><img src="http://placehold.it/100x150" alt="Libro"></a>
+				<a href="ejemplar.php"><img src="http://placehold.it/100x150" alt="Libro"></a>
+				<a href="ejemplar.php"><img src="http://placehold.it/100x150" alt="Libro"></a>
+				<a href="ejemplar.php"><img src="http://placehold.it/100x150" alt="Libro"></a>
+				<a href="ejemplar.php"><img src="http://placehold.it/100x150" alt="Libro"></a>
+				<a href="ejemplar.php"><img src="http://placehold.it/100x150" alt="Libro"></a>
+				<a href="ejemplar.php"><img src="http://placehold.it/100x150" alt="Libro"></a>
+				<div class="col-md-12">
+					<a href="genero.php" class="pull-right">Ver mas<span class="glyphicon glyphicon-chevron-right"></span></a>
+				</div>	
   			</div>
 		</div>
+	<!-- ******************** GENERO 2 ************************** -->		
 		<div class="panel panel-success">
   			<div class="panel-heading">
-    			<a href="genero.php"><h3 class="panel-title" id="grande">Genero 2</h3></a>
+    			<a href="genero.php"><h3 class="TituloGrande">Genero 2</h3> </a>
   			</div>
-  			<div class="panel-body">
-				<a href="ejemplar.php"><img src="http://placehold.it/100x100" alt="Libro"></a>
-				<a href="ejemplar.php"><img src="http://placehold.it/100x100" alt="Libro"></a>
-				<a href="ejemplar.php"><img src="http://placehold.it/100x100" alt="Libro"></a>
-				<a href="ejemplar.php"><img src="http://placehold.it/100x100" alt="Libro"></a>
-				<a href="ejemplar.php"><img src="http://placehold.it/100x100" alt="Libro"></a>
-				<a href="ejemplar.php"><img src="http://placehold.it/100x100" alt="Libro"></a>
-				<a href="ejemplar.php"><img src="http://placehold.it/100x100" alt="Libro"></a>
-				<a href="ejemplar.php"><img src="http://placehold.it/100x100" alt="Libro"></a>
-				<a href="ejemplar.php"><img src="http://placehold.it/100x100" alt="Libro"></a>
-				<a href="ejemplar.php"><img src="http://placehold.it/100x100" alt="Libro"></a>
+  			<div class="panel-body librosSeparados">
+				<a href="ejemplar.php"><img src="http://placehold.it/100x150" alt="Libro"></a>
+				<a href="ejemplar.php"><img src="http://placehold.it/100x150" alt="Libro"></a>
+				<a href="ejemplar.php"><img src="http://placehold.it/100x150" alt="Libro"></a>
+				<a href="ejemplar.php"><img src="http://placehold.it/100x150" alt="Libro"></a>
+				<a href="ejemplar.php"><img src="http://placehold.it/100x150" alt="Libro"></a>
+				<a href="ejemplar.php"><img src="http://placehold.it/100x150" alt="Libro"></a>
+				<a href="ejemplar.php"><img src="http://placehold.it/100x150" alt="Libro"></a>
+				<a href="ejemplar.php"><img src="http://placehold.it/100x150" alt="Libro"></a>
+				<a href="ejemplar.php"><img src="http://placehold.it/100x150" alt="Libro"></a>
+				<a href="ejemplar.php"><img src="http://placehold.it/100x150" alt="Libro"></a>
+				<div class="col-md-12">
+					<a href="genero.php" class="pull-right">Ver mas<span class="glyphicon glyphicon-chevron-right"></span></a>
+				</div>	
   			</div>
 		</div>
+	<!-- ******************** GENERO 3 ************************** -->		
 		<div class="panel panel-success">
   			<div class="panel-heading">
-    			<a href="genero.php"><h3 class="panel-title" id="grande">Genero 3</h3></a>
+    			<a href="genero.php"><h3 class="TituloGrande">Genero 3</h3></a>
   			</div>
-  			<div class="panel-body">
-				<a href="ejemplar.php"><img src="http://placehold.it/100x100" alt="Libro"></a>
-				<a href="ejemplar.php"><img src="http://placehold.it/100x100" alt="Libro"></a>
-				<a href="ejemplar.php"><img src="http://placehold.it/100x100" alt="Libro"></a>
-				<a href="ejemplar.php"><img src="http://placehold.it/100x100" alt="Libro"></a>
-				<a href="ejemplar.php"><img src="http://placehold.it/100x100" alt="Libro"></a>
-				<a href="ejemplar.php"><img src="http://placehold.it/100x100" alt="Libro"></a>
-				<a href="ejemplar.php"><img src="http://placehold.it/100x100" alt="Libro"></a>
-				<a href="ejemplar.php"><img src="http://placehold.it/100x100" alt="Libro"></a>
-				<a href="ejemplar.php"><img src="http://placehold.it/100x100" alt="Libro"></a>
-				<a href="ejemplar.php"><img src="http://placehold.it/100x100" alt="Libro"></a>
+  			<div class="panel-body librosSeparados">
+				<a href="ejemplar.php"><img src="http://placehold.it/100x150" alt="Libro"></a>
+				<a href="ejemplar.php"><img src="http://placehold.it/100x150" alt="Libro"></a>
+				<a href="ejemplar.php"><img src="http://placehold.it/100x150" alt="Libro"></a>
+				<a href="ejemplar.php"><img src="http://placehold.it/100x150" alt="Libro"></a>
+				<a href="ejemplar.php"><img src="http://placehold.it/100x150" alt="Libro"></a>
+				<a href="ejemplar.php"><img src="http://placehold.it/100x150" alt="Libro"></a>
+				<a href="ejemplar.php"><img src="http://placehold.it/100x150" alt="Libro"></a>
+				<a href="ejemplar.php"><img src="http://placehold.it/100x150" alt="Libro"></a>
+				<a href="ejemplar.php"><img src="http://placehold.it/100x150" alt="Libro"></a>
+				<a href="ejemplar.php"><img src="http://placehold.it/100x150" alt="Libro"></a>
+				<div class="col-md-12">
+					<a href="genero.php" class="pull-right">Ver mas<span class="glyphicon glyphicon-chevron-right"></span></a>
+				</div>	
   			</div>
 		</div>
+	<!-- ******************** GENERO 4 ************************** -->		
 		<div class="panel panel-success">
   			<div class="panel-heading">
-    			<a href="genero.php"><h3 class="panel-title" id="grande">Genero 4</h3></a>
+    			<a href="genero.php"><h3 class="TituloGrande">Genero 4</h3></a>
   			</div>
-  			<div class="panel-body">
-				<a href="ejemplar.php"><img src="http://placehold.it/100x100" alt="Libro"></a>
-				<a href="ejemplar.php"><img src="http://placehold.it/100x100" alt="Libro"></a>
-				<a href="ejemplar.php"><img src="http://placehold.it/100x100" alt="Libro"></a>
-				<a href="ejemplar.php"><img src="http://placehold.it/100x100" alt="Libro"></a>
-				<a href="ejemplar.php"><img src="http://placehold.it/100x100" alt="Libro"></a>
-				<a href="ejemplar.php"><img src="http://placehold.it/100x100" alt="Libro"></a>
-				<a href="ejemplar.php"><img src="http://placehold.it/100x100" alt="Libro"></a>
-				<a href="ejemplar.php"><img src="http://placehold.it/100x100" alt="Libro"></a>
-				<a href="ejemplar.php"><img src="http://placehold.it/100x100" alt="Libro"></a>
-				<a href="ejemplar.php"><img src="http://placehold.it/100x100" alt="Libro"></a>
+  			<div class="panel-body librosSeparados">
+				<a href="ejemplar.php"><img src="http://placehold.it/100x150" alt="Libro"></a>
+				<a href="ejemplar.php"><img src="http://placehold.it/100x150" alt="Libro"></a>
+				<a href="ejemplar.php"><img src="http://placehold.it/100x150" alt="Libro"></a>
+				<a href="ejemplar.php"><img src="http://placehold.it/100x150" alt="Libro"></a>
+				<a href="ejemplar.php"><img src="http://placehold.it/100x150" alt="Libro"></a>
+				<a href="ejemplar.php"><img src="http://placehold.it/100x150" alt="Libro"></a>
+				<a href="ejemplar.php"><img src="http://placehold.it/100x150" alt="Libro"></a>
+				<a href="ejemplar.php"><img src="http://placehold.it/100x150" alt="Libro"></a>
+				<a href="ejemplar.php"><img src="http://placehold.it/100x150" alt="Libro"></a>
+				<a href="ejemplar.php"><img src="http://placehold.it/100x150" alt="Libro"></a>
+				<div class="col-md-12">
+					<a href="genero.php" class="pull-right">Ver mas<span class="glyphicon glyphicon-chevron-right"></span></a>
+				</div>				
   			</div>
 		</div>
+	<!-- ******************** GENERO 5 ************************** -->		
 		<div class="panel panel-success">
   			<div class="panel-heading">
-    			<a href="genero.php"><h3 class="panel-title" id="grande">Genero 5</h3></a>
+    			<a href="genero.php"><h3 class="TituloGrande">Genero 5</h3></a>
   			</div>
-  			<div class="panel-body">
-				<a href="ejemplar.php"><img src="http://placehold.it/100x100" alt="Libro"></a>
-				<a href="ejemplar.php"><img src="http://placehold.it/100x100" alt="Libro"></a>
-				<a href="ejemplar.php"><img src="http://placehold.it/100x100" alt="Libro"></a>
-				<a href="ejemplar.php"><img src="http://placehold.it/100x100" alt="Libro"></a>
-				<a href="ejemplar.php"><img src="http://placehold.it/100x100" alt="Libro"></a>
-				<a href="ejemplar.php"><img src="http://placehold.it/100x100" alt="Libro"></a>
-				<a href="ejemplar.php"><img src="http://placehold.it/100x100" alt="Libro"></a>
-				<a href="ejemplar.php"><img src="http://placehold.it/100x100" alt="Libro"></a>
-				<a href="ejemplar.php"><img src="http://placehold.it/100x100" alt="Libro"></a>
-				<a href="ejemplar.php"><img src="http://placehold.it/100x100" alt="Libro"></a>
+  			<div class="panel-body librosSeparados">
+				<a href="ejemplar.php"><img src="http://placehold.it/100x150" alt="Libro"></a>
+				<a href="ejemplar.php"><img src="http://placehold.it/100x150" alt="Libro"></a>
+				<a href="ejemplar.php"><img src="http://placehold.it/100x150" alt="Libro"></a>
+				<a href="ejemplar.php"><img src="http://placehold.it/100x150" alt="Libro"></a>
+				<a href="ejemplar.php"><img src="http://placehold.it/100x150" alt="Libro"></a>
+				<a href="ejemplar.php"><img src="http://placehold.it/100x150" alt="Libro"></a>
+				<a href="ejemplar.php"><img src="http://placehold.it/100x150" alt="Libro"></a>
+				<a href="ejemplar.php"><img src="http://placehold.it/100x150" alt="Libro"></a>
+				<a href="ejemplar.php"><img src="http://placehold.it/100x150" alt="Libro"></a>
+				<a href="ejemplar.php"><img src="http://placehold.it/100x150" alt="Libro"></a>
+				<div class="col-md-12">
+					<a href="genero.php" class="pull-right">Ver mas<span class="glyphicon glyphicon-chevron-right"></span></a>
+				</div>	
   			</div>
 		</div>
 	</section>
-
+<!-- ******************** FIN SECTION ************************** -->
+<!-- ******************** INICIA ASIDE ************************** -->
 	<aside class="col-md-3 col-md-offset-2" >
+	<!-- ******************** MAS GENEROS ************************** -->	
 		<div class="panel panel-success">
 			<div class="panel-heading">
-	    		<h3 class="panel-title" id="Tbrowse"> Más géneros</h3>
+	    		<h3 class="TituloGrande" id="Tbrowse"> Más géneros</h3>
 			</div>
 			<div class="panel-body">
 				<ul>
@@ -134,6 +158,7 @@
 			</div>			
 		</div>
 	</aside>
-		<?php
-			include "includes/footer.html";
-		?>
+	<!-- ******************** FIN ASIDE ************************** -->
+<?php
+	include "includes/footer.html";
+?>
--- login.php
+++ login.php
@@ -1,38 +1,44 @@
 <?php
 	include "includes/navbar.html";
 ?>
+
 <div id="contenedor">
-	    <section id="formulario" class="col-md-4 col-md-offset-4">
-			<div class="panel panel-success" id="contenedorlogin">
+	<section id="formulario" class="col-md-4 col-md-offset-4">
+		<div class="panel panel-success">
 			<div class="panel-heading">
-	    		<h3 class="panel-title" id="Tlogin"> Inicia Sesion:</h3>
+				<h3 class="TituloGrande"> Inicia Sesion:</h3>
 			</div>
 			<div class="panel-body">
-				<form action="index.php" method="post" class="form-horizontal">
+				<form class="form-horizontal">
 					<div class="form-group">
-						<label for="usuario" class="col-lg-2 control-label" id="usuario" >Usuario:</label>
-						<div class="col-lg-8">
-						<input type="text" class="form-control" id="usuario" placeholder="USUARIO">
+						<label for="usuario" class="col-md-4 control-label" id="usuario" >Usuario:</label>
+						<div class="col-md-8">
+							<input type="text" class="form-control" id="usuario" placeholder="USUARIO">
 						</div>
 					</div>
 					<div class="form-group">
-					    <label for="pass" class="col-lg-2 control-label" id="pass">Contraseña:</label>
-					    <div class="col-lg-8">
-					    <input type="password" class="form-control" id="pass" placeholder="CONTRASEÑA">
+					    <label for="pass" class="col-md-4 control-label" id="pass">Contraseña:</label>
+					    <div class="col-md-8">
+					    	<input type="password" class="form-control" id="pass" placeholder="CONTRASEÑA">
 						</div>			      
 					</div>
-
 					<div class="form-group">
-						<div class="col-lg-10 col-lg-offset-2">
-							<button type="submit" class="btn btn-default" id="btnlogin">Entrar</button>
+						<div class="col-md-12">
+							<button type="submit" class="btn btn-default pull-right">Entrar</button>
 						</div>
-					</div>	
-					<a href="recupera.php" class="btn btn-link" id="recuperar">Recuperar contraseña</a>
-					<a href="registro.php" class="btn btn-link" id="registro">Registro</a>
+					</div>
+					<div class="form-group">
+						<div class="col-md-12">	
+							<a href="recupera.php" class="btn btn-link" id="recuperar">Recuperar contraseña</a>
+							<a href="registro.php" class="btn btn-link pull-right" id="registro">Registro</a>
+						</div>
+					</div>
 				</form>
-		</section>
-
+			</div>	
+		</div>		
+	</section>
 </div>		
-		<?php
-			include "includes/footer.html";
-		?>
+
+<?php
+	include "includes/footer.html";
+?>
