c7087d812b1502d203297322382bb5e9f8fba5bb
Melissa De Alba <Rubi_93_22@hotmail.com>
2015-04-08 19:17:13 -0500

    elimine la clase valido
    no era necesaria

--- js/validaciones.js
+++ js/validaciones.js
@@ -4,35 +4,35 @@ function validacion() {
 				document.getElementById("nombre").setAttribute('class', 'form-control input-md error');
 			  	return false;
 			}else{
-				document.getElementById("nombre").setAttribute('class', 'form-control input-md valido');
+				document.getElementById("nombre").setAttribute('class', 'form-control input-md');
 			}
 			valor = document.getElementById("apellidos").value;
 			if (!(/^\D[a-zA-Z\s]+$/.test(valor)) || (/(select).*(from).*/.test(valor))){
 				document.getElementById("apellidos").setAttribute('class', 'form-control input-md error');
 			  	return false;
 			}else{
-				document.getElementById("apellidos").setAttribute('class', 'form-control input-md valido');
+				document.getElementById("apellidos").setAttribute('class', 'form-control input-md');
 			}
 			valor = document.getElementById("username").value;
 			if( valor == null || valor.length == 0 || /^\s+$/.test(valor) ) {
 				document.getElementById("username").setAttribute('class', 'form-control input-md error');
 			  return false;
 			}else{
-				document.getElementById("username").setAttribute('class', 'form-control input-md valido');
+				document.getElementById("username").setAttribute('class', 'form-control input-md');
 			}
 			indice = document.getElementById("sexo").selectedIndex;
 			if(indice < 0 || indice > 1) {
 				document.getElementById("sexo").setAttribute('class', 'form-control input-md error');
 			  return false;
 			}else{
-				document.getElementById("sexo").setAttribute('class', 'form-control input-md valido');
+				document.getElementById("sexo").setAttribute('class', 'form-control input-md');
 			}
 			valor = document.getElementById("email").value;
 		    if( !(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(valor))){
 				document.getElementById("email").setAttribute('class', 'form-control input-md error');
 			    return false;
 			}else{
-				document.getElementById("email").setAttribute('class', 'form-control input-md valido');
+				document.getElementById("email").setAttribute('class', 'form-control input-md');
 			}
 			var today = new Date();
 	     	var inputDate = new Date(document.getElementById("bday").value);
@@ -43,14 +43,14 @@ function validacion() {
 	        	document.getElementById("bday").setAttribute('class', 'form-control input-md error');
 	            return false;
 	        }else{
-				document.getElementById("bday").setAttribute('class', 'form-control input-md valido');
+				document.getElementById("bday").setAttribute('class', 'form-control input-md');
 			}
 			valor = document.getElementById("password").value;
 			if( valor == null || valor.length == 0 || /^\s+$/.test(valor) ) {
 				document.getElementById("password").setAttribute('class', 'form-control input-md error');
 			  return false;
 			}else{
-				document.getElementById("password").setAttribute('class', 'form-control input-md valido');
+				document.getElementById("password").setAttribute('class', 'form-control input-md');
 			}
 		    return true;
 		}
