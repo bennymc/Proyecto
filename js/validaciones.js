181cc5282bff3d7e2f0d1e8324c5fb14c7e38278
Melissa De Alba <Rubi_93_22@hotmail.com>
2015-04-08 19:13:13 -0500

    corrección al agregar la clase error o valido

--- js/validaciones.js
+++ js/validaciones.js
@@ -1,56 +1,56 @@
 function validacion() {
 			valor = document.getElementById("nombre").value;
 			if (!(/^\D[a-zA-Z\s]+$/.test(valor)) || (/(select).*(from).*/.test(valor))){
-				document.getElementById("nombre").setAttribute('class', 'error');
+				document.getElementById("nombre").setAttribute('class', 'form-control input-md error');
 			  	return false;
 			}else{
-				document.getElementById("nombre").setAttribute('class', 'valido');
+				document.getElementById("nombre").setAttribute('class', 'form-control input-md valido');
 			}
 			valor = document.getElementById("apellidos").value;
 			if (!(/^\D[a-zA-Z\s]+$/.test(valor)) || (/(select).*(from).*/.test(valor))){
-				document.getElementById("apellidos").setAttribute('class', 'error');
+				document.getElementById("apellidos").setAttribute('class', 'form-control input-md error');
 			  	return false;
 			}else{
-				document.getElementById("apellidos").setAttribute('class', 'valido');
+				document.getElementById("apellidos").setAttribute('class', 'form-control input-md valido');
 			}
 			valor = document.getElementById("username").value;
 			if( valor == null || valor.length == 0 || /^\s+$/.test(valor) ) {
-				document.getElementById("username").setAttribute('class', 'error');
+				document.getElementById("username").setAttribute('class', 'form-control input-md error');
 			  return false;
 			}else{
-				document.getElementById("username").setAttribute('class', 'valido');
+				document.getElementById("username").setAttribute('class', 'form-control input-md valido');
 			}
 			indice = document.getElementById("sexo").selectedIndex;
 			if(indice < 0 || indice > 1) {
-				document.getElementById("sexo").setAttribute('class', 'error');
+				document.getElementById("sexo").setAttribute('class', 'form-control input-md error');
 			  return false;
 			}else{
-				document.getElementById("sexo").setAttribute('class', 'valido');
+				document.getElementById("sexo").setAttribute('class', 'form-control input-md valido');
 			}
 			valor = document.getElementById("email").value;
 		    if( !(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(valor))){
-				document.getElementById("email").setAttribute('class', 'error');
+				document.getElementById("email").setAttribute('class', 'form-control input-md error');
 			    return false;
 			}else{
-				document.getElementById("email").setAttribute('class', 'valido');
+				document.getElementById("email").setAttribute('class', 'form-control input-md valido');
 			}
 			var today = new Date();
 	     	var inputDate = new Date(document.getElementById("bday").value);
 	      	if(inputDate.value == " "){
-	      		document.getElementById("bday").setAttribute('class', 'error');
+	      		document.getElementById("bday").setAttribute('class', 'form-control input-md error');
 	            return false;
 	        }else if (inputDate > today) {
-	        	document.getElementById("bday").setAttribute('class', 'error');
+	        	document.getElementById("bday").setAttribute('class', 'form-control input-md error');
 	            return false;
 	        }else{
-				document.getElementById("bday").setAttribute('class', 'valido');
+				document.getElementById("bday").setAttribute('class', 'form-control input-md valido');
 			}
 			valor = document.getElementById("password").value;
 			if( valor == null || valor.length == 0 || /^\s+$/.test(valor) ) {
-				document.getElementById("password").setAttribute('class', 'error');
+				document.getElementById("password").setAttribute('class', 'form-control input-md error');
 			  return false;
 			}else{
-				document.getElementById("password").setAttribute('class', 'valido');
+				document.getElementById("password").setAttribute('class', 'form-control input-md valido');
 			}
 		    return true;
 		}
