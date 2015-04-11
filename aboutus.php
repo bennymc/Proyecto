91920a84fd3ec19a98fba45ec4f353c635badc59
Melissa De Alba <Rubi_93_22@hotmail.com>
2015-04-10 21:59:49 -0500

    correccion en link de titulo en perfil

--- aboutus.php
+++ aboutus.php
@@ -1,10 +1,11 @@
 <?php
 	include "includes/navbar.html";
 ?>
-<div id="contenedor">		
-		<div class="panel panel-info col-md-4 col-md-offset-4" id="aboutus">
+<div id="contenedor">	
+	<section class="col-md-4 col-md-offset-4">
+		<div class="panel panel-info " id="aboutus">
 		  <div class="panel-heading">
-		    <h3 class="panel-title">CREADORES</h3>
+		    <h3 class="TituloGrande">CREADORES</h3>
 		  </div>
 		  <div class="panel-body">
 		    <p>Integrantes del equipo: </p>
@@ -12,6 +13,7 @@
 		    <p>Melissa Rubi De Alba Mendoza</p>
 		  </div>
 		</div>
+		</section>
 </div>
 <?php
 	include "includes/footer.html";
--- js/datos.js
+++ js/datos.js
@@ -323,7 +323,7 @@ function CargarPerfil(){
 				$('#Libro'+cont).find('img').attr('src',json.Libros[n].Imagen);
 				var titulo=json.Libros[n].Titulo.slice(0,10)+'...';
 				$('#Libro'+cont).find('a').eq(0).text(titulo);
-				$('#Libro'+cont).find('a').eq(1).attr("href", "ejemplar.php?titulo=" + json.Libros[n].Titulo);
+				$('#Libro'+cont).find('a').eq(0).attr("href", "ejemplar.php?titulo=" + json.Libros[n].Titulo);
 				if(Status[x]== 1){
 					$('#Libro'+cont).find('a').eq(2).text('Leído');
 				}
