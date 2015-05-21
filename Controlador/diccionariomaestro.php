e76a36ae9897e044117f80eaebdcff8a1aa3c740
Melissa De Alba <Rubi_93_22@hotmail.com>
2015-05-21 16:52:38 -0500

    agrege al diccionaro la carga del menu con y sin sesion
    actualmente visible en inicio y regisro

--- Controlador/diccionariomaestro.php
+++ Controlador/diccionariomaestro.php
@@ -13,19 +13,30 @@ class diccionarioM{
 				//var_dump($link);
 				$diccionario = array(
 									'{USER}' => $_SESSION['usuario'],
-									'{LINKPERFIL}' => $link
-									//'{MJS}'=> "",
-									//'{END MJS}' => ""
+									'{LINKPERFIL}' => $link,
+									'{MENSAJE}'=> "",
+									'{ENDMENSAJE}' => ""
 									);
 				$header = strtr($header,$diccionario);
 
 			}
 			else
 				{
+
+					$i = strpos($header,'{MENSAJE');
+					$f = strpos($header, '}',$i);
+					$ff = strpos($header, '{ENDMENSAJE}',$f);
+					
+					$linkmsj = substr($header, $f+2,$ff-($f+2));
+
+					$header = str_replace($linkmsj,"",$header);
+
 					$diccionario = array(
-					'{USER}' => "Inicia Sesion",
-					'{LINKPERFIL}' => "?ctl=inicio"
-					);
+								'{USER}' => "Inicia Sesion",
+								'{LINKPERFIL}' => "?ctl=inicio",
+								'{MENSAJE}'=> "",
+								'{ENDMENSAJE}' => ""
+								);
 					$header = strtr($header,$diccionario);
 				}
 		
@@ -49,37 +60,21 @@ class diccionarioM{
 	
 	function CargarRegistro(){
 
+				$this->CargarHeader();
 				$vista = file_get_contents("Vista/registro.php");
-				$header = file_get_contents("Vista/navbar.html");
 				$footer = file_get_contents("Vista/footer.html");
-				$vista = $header . $vista . $footer;
-				//Reemplazo con un diccionario
-				$diccionario = array(
-									'{USER}' => "Inicia Sesion",
-									'{LINKPERFIL}' => "?ctl=inicio"
-									);
-				$vista= strtr($vista,$diccionario);
+				$vista = $this->headerfinal  . $vista . $footer;
 
 
 				echo $vista;
 	}
 	function CargarInicioWSesion(){
 		
-					
-					$vista  = file_get_contents("Vista/inicio.html");
-					$header = file_get_contents("Vista/navbar.html");
+					$this->CargarHeader();
+
+					$vista  = file_get_contents("Vista/inicio.html");					
 					$footer = file_get_contents("Vista/footer.html");
-					$vista  = $header . $vista . $footer;
-					//Reemplazo con un diccionario	
-					$link =     "?ctl=perfil&id=".$_SESSION['idUsuario'];
-					//var_dump($link);
-					$diccionario = array(
-										'{USER}' => $_SESSION['usuario'],
-										'{LINKPERFIL}' => $link
-										);
-					$vista= strtr($vista,$diccionario);	
-
-
+					$vista = $this->headerfinal  . $vista . $footer;
 					echo $vista;
 	}
 
