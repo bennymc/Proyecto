88e085dee6996f041bc0e5216a70acfd422d427d
Bennymc <benny_9_3@hotmail.com>
2015-05-22 22:05:13 -0500

    Mostrar libros en las páginas de géneros.

--- Controlador/generosCtl.php
+++ Controlador/generosCtl.php
@@ -21,15 +21,16 @@ class GenerosCtl{
 			$footer = file_get_contents("Vista/footer.html");
 			$vista = strtr($vista, $diccionario);
 
-			$i = strpos($vista,'{{repite');
+			$i = strpos($vista,'{{repite generos');
 			$f = strpos($vista, '}}', $i);
-			$ff = strpos($vista, '{{termina repite}}', $f);
-			$bloque = substr($vista, $i,$f-($ff-4));
-			//echo $bloque;
+			$ff = strpos($vista, '{{termina repite1}}', $f);
+
+			$bloque = substr($vista, $i,$ff-$i+19);
+
 			$repetir_cad = substr($vista, $f+2, ($ff-16)-$f);
 			//echo $repetir_cad;
 			$vista = str_replace($bloque,"",$vista);
-			
+
 			$generos="";
 
 			for($x=0; $x < count($this->mdl->Generos); $x++) {
@@ -42,11 +43,39 @@ class GenerosCtl{
 				$generos = $generos.$aux."</br>";
 			}
 			
-			
-			
 		    $vista = str_replace("{{GENEROS}}",$generos,$vista);
+			
+
+
+			$i = strpos($vista,'{{repite libros');
+			$f = strpos($vista, '}}', $i);
+			$ff = strpos($vista, '{{termina repite2}}', $f);
+			$bloque = substr($vista, $i,$ff-$i+19);
+			//var_dump($bloque);
+
+			$repetir_cad = substr($vista, $f+2, $ff-$f-2);
+			//echo $repetir_cad;
+			
+			$vista = str_replace($bloque,"",$vista);
+			
+			$libros="";
+
+			for($x=0; $x < count($this->mdl->idLibros); $x++) {
+				$diccionarioLibros= array(
+									'{{titulo}}' => $this->mdl->nombreLibros[$x],
+									'{{id}}' => $this->mdl->idLibros[$x],
+									'{{url}}' => $this->mdl->urlLibros[$x]
+										);
+				$aux = $repetir_cad;
+				$aux = strtr($aux,$diccionarioLibros);
+				$libros = $libros.$aux;
+			}
+			
+		    $vista = str_replace("{{LIBROS}}",$libros,$vista);
 			$vista = $this->dicc->headerfinal . $vista. $footer;
-			//Mostrar la vista
+
+			
+
 			echo $vista;
 		}else
 		{
--- Modelo/GenerosMdl.php
+++ Modelo/GenerosMdl.php
@@ -5,6 +5,9 @@ class GenerosMdl{
 	public $CabeceraLibrero;
 	public $Descripcion;
 	public $Generos;
+	public $idLibros;
+	public $nombreLibros;
+	public $urlLibros;
 	public $Datos;
 
 	function show($id){
@@ -33,6 +36,33 @@ class GenerosMdl{
 		}
 		while($fila=$resultado->fetch_assoc()){
 			$this->Generos[]=$fila["nombre"];
+		}
+
+		$consulta = "SELECT idLibros
+				 FROM generos_has_libros
+				 WHERE idGeneros = '".$id."'";
+		$resultado = $conexion->query($consulta);
+		if($conexion->errno){
+			die("Tu query tiene un error
+				<br>$conexion->error");
+		}
+		while($fila=$resultado->fetch_assoc()){
+			$this->idLibros[]=$fila["idLibros"];
+		}
+
+		foreach ($this->idLibros as $id) {
+			$consulta = "SELECT titulo, imagen_portada
+			 FROM libros
+			 WHERE idLibros = '".$id."'";
+			$resultado = $conexion->query($consulta);
+			if($conexion->errno){
+				die("Tu query tiene un error
+					<br>$conexion->error");
+			}
+			while($fila=$resultado->fetch_assoc()){
+				$this->nombreLibros[]=$fila["titulo"];
+				$this->urlLibros[]=$fila["imagen_portada"];
+			}	
 		}
 		$conexion->close();	
 	}
