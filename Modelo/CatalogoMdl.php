033ef65204ca5aa09afcbf1661779ccb5f97aa0e
Melissa De Alba <Rubi_93_22@hotmail.com>
2015-05-24 16:44:07 -0500

    carga genreros random al catalogo

--- Modelo/CatalogoMdl.php
+++ Modelo/CatalogoMdl.php
@@ -2,6 +2,8 @@
 
 class CatalogoMdl{
 	public $Generos;
+	public $nombresR;
+	public $random;
 
 	function show(){
 		require('config.inc');
@@ -23,6 +25,51 @@ class CatalogoMdl{
 		}
 		$conexion->close();	
 	}
+
+	function GenerosRandom(){
+		require('config.inc');
+		$conexion = new mysqli($servidor,$usuario,$pass,$bd);
+		if($conexion -> connect_errno){
+			echo "Hubo un error";
+			echo "<br>$conexion->connect_errno";
+		}
+
+
+		$random[0]=rand(1,24);
+		$x=1;
+		while($x<5){
+			$y = rand(1,24);
+			if(!in_array($y, $random)){
+				$random[$x]=$y;
+				$x++;
+			}
+		}
+
+		$consulta = "SELECT nombre
+				 FROM generos
+				 WHERE idGeneros 
+				 IN (
+				 	\"$random[0]\",
+				 	\"$random[1]\",
+				 	\"$random[2]\",
+				 	\"$random[3]\",
+				 	\"$random[4]\"
+				 	)";
+		$resultado = $conexion->query($consulta);
+
+		if($conexion->errno){
+			die("Tu query tiene un error
+				<br>$conexion->error");
+		}
+		while($fila=$resultado->fetch_assoc()){
+			$this->nombresR[]=$fila["nombre"];
+		}
+		
+		$conexion->close();	
+	}
+
+
+
 }
 
 ?>
