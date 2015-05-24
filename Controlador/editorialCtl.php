63f4c44cc904f768229986be63c0d213444075d5
Bennymc <benny_9_3@hotmail.com>
2015-05-23 19:20:58 -0500

    EditorialGenero ya funciona.

--- Controlador/editorialCtl.php
+++ Controlador/editorialCtl.php
@@ -10,7 +10,8 @@ class editorialCtl{
 	}
 	public function ejecutar(){
 		if(isset($_GET['id']) && $this->validateInteger($_GET['id'])){
-			$this->mdl->show($_GET['id']);
+			$idEditorial = $_GET['id'];
+			$this->mdl->show($idEditorial);
 			$diccionario = array(
 				'{{Titulo}}' => $this->mdl->Titulo);
 
@@ -34,7 +35,8 @@ class editorialCtl{
 			for($x=0; $x < count($this->mdl->Generos); $x++) {
 				$diccionarioGeneros= array(
 									'{{genero}}' => $this->mdl->Generos[$x],
-									'{{numero}}' => $x+1
+									'{{idGenero}}' => $x+1,
+									'{{idEditorial}}' => $idEditorial
 										);
 				$aux = $repetir_cad;
 				$aux = strtr($aux,$diccionarioGeneros);
@@ -90,15 +92,84 @@ class editorialGeneroCtl{
 	function __construct(){
 		require_once("Modelo/editorialMdl.php");
 		$this->mdl=new editorialMdl();
+		require_once("Controlador/diccionariomaestro.php");
+			$this->dicc = new diccionarioM(); 
 	}
 	public function ejecutar(){
-		//if(isset($_GET['id']) && $this->validateInteger($_GET['id'])){
-		//	$this->mdl->show($_GET['id']);
-			require_once("Vista/editorialGenero.php");
-		/*}else
+		if(isset($_GET['idEditorial']) && $this->validateInteger($_GET['idEditorial']) && isset($_GET['idGenero']) && $this->validateInteger($_GET['idGenero'])){
+			$idEditorial = $_GET['idEditorial'];
+			$idGenero = $_GET['idGenero'];
+			$this->mdl->show2($idEditorial, $idGenero);
+			$diccionario = array(
+				'{{Editorial}}' => $this->mdl->Nombre,
+				'{{Genero}}' => $this->mdl->nombreGenero);
+
+			$vista = file_get_contents("Vista/editorialGenero.html");
+			$this->dicc->CargarHeader();			
+			$footer = file_get_contents("Vista/footer.html");
+			$vista = strtr($vista, $diccionario);
+
+			$i = strpos($vista,'{{repite generos');
+			$f = strpos($vista, '}}', $i);
+			$ff = strpos($vista, '{{termina repite1}}', $f);
+
+			$bloque = substr($vista, $i,$ff-$i+19);
+
+			$repetir_cad = substr($vista, $f+2, ($ff-16)-$f);
+			//echo $repetir_cad;
+			$vista = str_replace($bloque,"",$vista);
+
+			$generos="";
+
+			for($x=0; $x < count($this->mdl->Generos); $x++) {
+				$diccionarioGeneros= array(
+									'{{genero}}' => $this->mdl->Generos[$x],
+									'{{idGenero}}' => $x+1,
+									'{{idEditorial}}' => $idEditorial
+										);
+				$aux = $repetir_cad;
+				$aux = strtr($aux,$diccionarioGeneros);
+				$generos = $generos.$aux."</br>";
+			}
+			
+		    $vista = str_replace("{{GENEROS}}",$generos,$vista);
+			
+
+
+			$i = strpos($vista,'{{repite libros');
+			$f = strpos($vista, '}}', $i);
+			$ff = strpos($vista, '{{termina repite2}}', $f);
+			$bloque = substr($vista, $i,$ff-$i+19);
+			//var_dump($bloque);
+
+			$repetir_cad = substr($vista, $f+2, $ff-$f-2);
+			//echo $repetir_cad;
+			
+			$vista = str_replace($bloque,"",$vista);
+			
+			$libros="";
+
+			for($x=0; $x < count($this->mdl->idLibros); $x++) {
+				$diccionarioLibros= array(
+									'{{titulo}}' => $this->mdl->nombreLibros[$x],
+									'{{id}}' => $this->mdl->idLibros[$x],
+									'{{url}}' => $this->mdl->urlLibros[$x]
+										);
+				$aux = $repetir_cad;
+				$aux = strtr($aux,$diccionarioLibros);
+				$libros = $libros.$aux;
+			}
+			
+		    $vista = str_replace("{{LIBROS}}",$libros,$vista);
+			$vista = $this->dicc->headerfinal . $vista. $footer;
+
+			
+
+			echo $vista;
+		}else
 		{
 			http_response_code(404);
-		}*/
+		}
 	}
 	function validateInteger($valor){
 		return is_int((int)$valor);
--- Modelo/editorialMdl.php
+++ Modelo/editorialMdl.php
@@ -3,6 +3,7 @@
 class editorialMdl{
 	public $Descripcion;
 	public $Generos;
+	public $nombreGenero;
 	public $idLibros;
 	public $nombreLibros;
 	public $urlLibros;
@@ -60,6 +61,81 @@ class editorialMdl{
 				$this->nombreLibros[]=$fila["titulo"];
 				$this->urlLibros[]=$fila["imagen_portada"];
 			}	
+		}
+		$conexion->close();	
+	}
+
+	function show2($idEditorial, $idGenero){
+		require_once('config.inc');
+		$conexion = new mysqli($servidor,$usuario,$pass,$bd);
+		if($conexion -> connect_errno){
+			echo "Hubo un error";
+			echo "<br>$conexion->connect_errno";
+		}
+
+		$consulta = "SELECT descripcion, nombre
+				 FROM editoriales 
+				 WHERE idEditoriales = '".$idEditorial."'";
+		$resultado = $conexion->query($consulta);
+		$this->Datos = $resultado->fetch_row();
+		$this->Descripcion = $this->Datos[0];
+		$this->Nombre = $this->Datos[1];
+
+		if($conexion->errno){
+			die("Tu query tiene un error
+				<br>$conexion->error");
+		}
+
+		$consulta = "SELECT nombre
+				 FROM generos
+				 WHERE idGeneros = $idGenero";
+		$resultado = $conexion->query($consulta);
+		$this->Datos = $resultado->fetch_row();
+		$this->nombreGenero = $this->Datos[0];
+
+		if($conexion->errno){
+			die("Tu query tiene un error
+				<br>$conexion->error");
+		}
+
+		$consulta = "SELECT nombre
+				 FROM generos";
+		$resultado = $conexion->query($consulta);
+		if($conexion->errno){
+			die("Tu query tiene un error
+				<br>$conexion->error");
+		}
+		while($fila=$resultado->fetch_assoc()){
+			$this->Generos[]=$fila["nombre"];
+		}
+
+		$consulta = "SELECT EHL.idLibros 
+				 FROM editoriales_has_libros EHL
+				 JOIN generos_has_libros GHL on EHL.idLibros = GHL.idLibros 
+				 WHERE idEditoriales = '".$idEditorial."' AND idGeneros = '".$idGenero."'";
+		$resultado = $conexion->query($consulta);
+		if($conexion->errno){
+			die("Tu query tiene un error
+				<br>$conexion->error");
+		}
+		while($fila=$resultado->fetch_assoc()){
+			$this->idLibros[]=$fila["idLibros"];
+		}
+		if(count($this->idLibros) > 0){
+			foreach ($this->idLibros as $id) {
+				$consulta = "SELECT titulo, imagen_portada
+				 FROM libros
+				 WHERE idLibros = '".$id."'";
+				$resultado = $conexion->query($consulta);
+				if($conexion->errno){
+					die("Tu query tiene un error
+						<br>$conexion->error");
+				}
+				while($fila=$resultado->fetch_assoc()){
+					$this->nombreLibros[]=$fila["titulo"];
+					$this->urlLibros[]=$fila["imagen_portada"];
+				}	
+			}
 		}
 		$conexion->close();	
 	}
