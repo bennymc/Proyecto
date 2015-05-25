43f50a52bdc71bb564ddda915ea23b5ca0eedcf7
Bennymc <benny_9_3@hotmail.com>
2015-05-25 15:02:28 -0500

    Commit secreto

--- Controlador/inicioCtl.php
+++ Controlador/inicioCtl.php
@@ -156,41 +156,22 @@ class recuperaCtl{
 				//instanciamos un objeto de la clase phpmailer al que llamamos 
 				//por ejemplo mail
 				$mail = new PHPMailer();
-
-				//Con la propiedad Mailer le indicamos que vamos a usar un 
-				//servidor smtp
+				$mail->IsSendmail();
+				$mail->Mailer = "smtp";
 				$mail->IsSMTP();
-
-				//Asignamos a Host el nombre de nuestro servidor smtp
+				$mail->SMTPDebug = 3;
+				$mail->Timeout=30;
 				$mail->Host = $emailhost;
-
-				//Le indicamos que el servidor smtp requiere autenticación
 				$mail->SMTPAuth = true;
-
-				//Le decimos cual es nuestro nombre de usuario y password
 				$mail->Username = $emailusername;
 				$mail->Password = $emailpassword;
-
-				//Indicamos cual es nuestra dirección de correo y el nombre que 
-				  //queremos que vea el usuario que lee nuestro correo
+				$mail->Port = $emailpuerto;
+				$mail->SMTPSecure = '';
 				  $mail->From = "support@book2book.tk";
 				  $mail->FromName = "Support de Book2";
-
-				  //el valor por defecto 10 de Timeout es un poco escaso dado que voy a usar 
-				  //una cuenta gratuita, por tanto lo pongo a 30  
-				  $mail->Timeout=30;
-
-				  //Indicamos cual es la dirección de destino del correo
 				  $mail->AddAddress($email);
-
-				  //Asignamos asunto y cuerpo del mensaje
-				  //El cuerpo del mensaje lo ponemos en formato html, haciendo 
-				  //que se vea en negrita
 				  $mail->Subject = "Recuperar contraseña";
 				  $mail->Body = 'Hola, <br><br>Tu nueva contraseña es: '.$token.' <br><br> Una vez autentificad@ podrás cambiarla por una nueva. <br><br> Enlace al sitio http://www.book2book.tk/?ctl=inicio<br/><br/>';
-
-				  //se envia el mensaje, si no ha habido problemas 
-				  //la variable $exito tendra el valor true
 				  $exito = $mail->Send();
 						
 				   if(!$exito)
