29aeb82f6bff8fc6a2d812ad449ae9c37a34913d
Melissa De Alba <Rubi_93_22@hotmail.com>
2015-05-26 08:12:23 -0500

    face y twiter en potra ventana

--- Vista/aboutus.php
+++ Vista/aboutus.php
@@ -18,9 +18,9 @@
 			    <a  href="?ctl=usuario&id=2">
     				<img  src= "http://i.imgur.com/CVC7ubZ.png"  WIDTH=50  HEIGHT=50  alt="book2">
   				</a>
-			    <a href="https://www.facebook.com/YuukoHL">
+			    <a href="https://www.facebook.com/YuukoHL" target="_blank">
     				<img  src= "http://i.imgur.com/bZ4xU3E.png"  WIDTH=50  HEIGHT=50  alt="facebook"></a>
-    			<a  href="https://twitter.com/yuukosp">
+    			<a  href="https://twitter.com/yuukosp" target="_blank">
     				<img  src= "http://i.imgur.com/jGswmuN.png"  WIDTH=50  HEIGHT=50  alt="twitter">
   				</a>
   				
@@ -31,7 +31,7 @@
 			    <div class="col-md-5">
 		  		<h1 class="titulosenperfil" id="Nombre"> Juan Bernardo Morales C. </h1>
 		  		<p><br></p>
-		  		<a href="?ctl=usuario&id=2">
+		  		<a href="?ctl=usuario&id=1">
 			    <img class="center-block" id="fotodeperfil" src= "http://i.imgur.com/RveBjw2.png?1"  alt="imagen de perfil">
 			    </a>
 			    <h3>Ing. Computación</h3>
@@ -40,7 +40,7 @@
 			    <a  href="?ctl=usuario&id=1">
     				<img  src= "http://i.imgur.com/CVC7ubZ.png"  WIDTH=50  HEIGHT=50  alt="book2">
   				</a>
-			    <a href="https://www.facebook.com/benny.moralesc">
+			    <a href="https://www.facebook.com/benny.moralesc" target="_blank">
     				<img  src= "http://i.imgur.com/bZ4xU3E.png"  WIDTH=50  HEIGHT=50  alt="facebook"></a>
     			
   				
