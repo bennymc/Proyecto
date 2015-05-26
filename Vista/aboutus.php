8bd694e104d29dc1f1ab1c199e3be0f0786e581f
Melissa De Alba <Rubi_93_22@hotmail.com>
2015-05-26 07:53:54 -0500

    abaut us

--- Vista/aboutus.php
+++ Vista/aboutus.php
@@ -1,15 +1,51 @@
 
 <div id="contenedor">	
-	<section class="col-md-4 col-md-offset-4">
-		<div class="panel panel-info " id="aboutus">
+	<section class="col-md-10 col-md-offset-1">
+		<div class="panel panel-success " id="aboutus">
 		  <div class="panel-heading">
 		    <h3 class="TituloGrande">CREADORES</h3>
 		  </div>
 		  <div class="panel-body">
-		    <p>Integrantes del equipo: </p>
-		    <p>Juan Bernardo Morales Castañeda</p>
-		    <p>Melissa Rubi De Alba Mendoza</p>
-		  </div>
+		  	<div class="col-md-5">
+		  		<h1 class="titulosenperfil" id="Nombre"> Melissa Rubi De Alba M. </h1>
+		  		<p><br></p>
+		  		<a href="?ctl=usuario&id=2">
+			    <img class="center-block" id="fotodeperfil" src= "http://i.imgur.com/14nk6M0.png?1"  alt="imagen de perfil">
+			    </a>
+			    <h3>Lic.informática</h3>
+			    <h3>Contacto:</h3>
+			    <h3><span class=" glyphicon glyphicon-envelope"></span> :Rubi_93_22@hotmail.com</h3>
+			    <a  href="?ctl=usuario&id=2">
+    				<img  src= "http://i.imgur.com/CVC7ubZ.png"  WIDTH=50  HEIGHT=50  alt="book2">
+  				</a>
+			    <a href="https://www.facebook.com/YuukoHL">
+    				<img  src= "http://i.imgur.com/bZ4xU3E.png"  WIDTH=50  HEIGHT=50  alt="facebook"></a>
+    			<a  href="https://twitter.com/yuukosp">
+    				<img  src= "http://i.imgur.com/jGswmuN.png"  WIDTH=50  HEIGHT=50  alt="twitter">
+  				</a>
+  				
+			</div>
+			<div class="col-md-2">
+			</div>
+			
+			    <div class="col-md-5">
+		  		<h1 class="titulosenperfil" id="Nombre"> Juan Bernardo Morales C. </h1>
+		  		<p><br></p>
+		  		<a href="?ctl=usuario&id=2">
+			    <img class="center-block" id="fotodeperfil" src= "http://i.imgur.com/RveBjw2.png?1"  alt="imagen de perfil">
+			    </a>
+			    <h3>Ing. Computación</h3>
+			    <h3>Contacto:</h3>
+			    <h3><span class=" glyphicon glyphicon-envelope"></span> :Benny_9_3@hotmail.com</h3>
+			    <a  href="?ctl=usuario&id=1">
+    				<img  src= "http://i.imgur.com/CVC7ubZ.png"  WIDTH=50  HEIGHT=50  alt="book2">
+  				</a>
+			    <a href="https://www.facebook.com/benny.moralesc">
+    				<img  src= "http://i.imgur.com/bZ4xU3E.png"  WIDTH=50  HEIGHT=50  alt="facebook"></a>
+    			
+  				
+			</div>
+			     
 		</div>
 		</section>
 </div>
